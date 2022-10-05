4de1b3dcfef4ab5897c62ac4796a541ed7e29321
Sourcery AI <>
2022-10-05 08:44:04 +0000

    'Refactored by Sourcery'

--- src/main.py
+++ src/main.py
@@ -12,13 +12,13 @@ def main():
     path = p.resolve()
     maxPages = 100
     convertPdfs(path)
-    analyzeJpgs(path, maxPages) 
+    analyzeJpgs(path, maxPages)
     #analyzeJpgFile('IMSLP00716-Schumann_-_Humoreske,_Op_20-001', path)
 
     #timer
     e2 = cv.getTickCount()
     time = (e2 - e1)/ cv.getTickFrequency()
-    print('Execution finished at total: ' + str(time))
+    print(f'Execution finished at total: {str(time)}')
 
 
 
